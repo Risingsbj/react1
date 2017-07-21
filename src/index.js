5da49be12366c7f1dd4d57888c2ca51395a51ebf
risingsun <243281121@qq.com>
2017-07-21 22:08:07 +0800

    component

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import Welcome from "./Welcome"
+
 
 ReactDOM.render(
-  <h1>Hi, world!</h1>,
-  document.getElementById('root2')
-);
+  <Welcome/>,
+  document.getElementById('root')
+)
